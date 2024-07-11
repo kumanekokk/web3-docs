677698717a986ccc1c3159e0a60e27a43076dad8
root <info@kumanekokk.com>
2024-07-12 06:32:33 +0900

    VOICEBOX

---
+++ js/common.js
@@ -0,0 +1,4 @@
+const footerLink = document.querySelector('footer div p');
+footerLink.setAttribute('onclick', 'location.href="https://kumanekokk.com"');
+footerLink.style.color = '#00f';
+footerLink.style.cursor = 'pointer';
